fa86bb55f925b6b4b6f6861568b48defeeda8869
Konstantin Tennhard <me@t6d.de>
2013-09-24 14:43:11 +0300

    Adds _model suffix to all model packaging helpers

--- lib/opennlp/model_packaging_helpers.rb
+++ lib/opennlp/model_packaging_helpers.rb
@@ -1,25 +1,25 @@
 module OpenNLP
   module ModelPackagingHelpers
     module SentenceDetection
-      def sentence_detection
+      def sentence_detection_model
         SentenceModel.load(File.join(root, 'support', 'tokenization.bin'))
       end
     end
 
     module Tokenization
-      def tokenization
+      def tokenization_model
         TokenizerModel.load(File.join(root, 'support', 'tokenization.bin'))
       end
     end
 
     module PosTagging
-      def pos_tagging
+      def pos_tagging_model
         POSModel.load(File.join(root, 'support', 'pos_tagging.bin'))
       end
     end
 
     module NamedEntityRecognition
-      def named_entity_recognition(entity_type)
+      def named_entity_recognition_model(entity_type)
         TokenNameFinderModel.load(File.join(root, 'support', 'named_entity_recognition', "#{entity_type}.bin"))
       end
     end
