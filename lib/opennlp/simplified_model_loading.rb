5ecd4397960e93b164a774cec77434f2317a8859
Konstantin Tennhard <me@t6d.de>
2013-09-24 12:48:09 +0300

    Better error handling for SimpliefiedModelLoading

--- lib/opennlp/simplified_model_loading.rb
+++ lib/opennlp/simplified_model_loading.rb
@@ -1,7 +1,7 @@
 module OpenNLP
   module SimplifiedModelLoading
     def load(path)
-      raise ArgumentError, "File not found" unless File.file?(path) and File.readable?(path)
+      raise ArgumentError, "File not found: #{path}" unless File.file?(path) and File.readable?(path)
       new Java::java.io.FileInputStream.new(path)
     end
   end
